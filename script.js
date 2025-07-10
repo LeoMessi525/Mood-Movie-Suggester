a1fa79848c526f42426ee2c1b4e691149f1d2eb3
Ashok Dahal <ashokdahal525@gmail.com>
2025-07-10 19:56:26 +0200

    Movies according to mood with posters

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 const movieSuggestions = {
-  happy: ["The Secret Life of Walter Mitty", "Amélie", "Paddington 2"],
-  sad: ["The Pursuit of Happyness", "Blue Valentine", "A Silent Voice"],
-  bored: ["Inception", "Mad Max: Fury Road", "Everything Everywhere All at Once"],
-  inspired: ["The Social Network", "Steve Jobs", "Whiplash"]
+  happy: [
+    {
+      title: "The Secret Life of Walter Mitty",
+      image: "https://m.media-amazon.com/images/I/91U4U+SdJRL._AC_SY679_.jpg"
+    },
+    {
+      title: "Amélie",
+      image: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg"
+    },
+    {
+      title: "Paddington 2",
+      image: "https://m.media-amazon.com/images/I/81Y8K6biYJL._AC_SY679_.jpg"
+    }
+  ],
+  sad: [
+    {
+      title: "The Pursuit of Happyness",
+      image: "https://m.media-amazon.com/images/I/71ePbXQkZ0L._AC_SY679_.jpg"
+    },
+    {
+      title: "Blue Valentine",
+      image: "https://m.media-amazon.com/images/I/81wzRI+J1bL._AC_SY679_.jpg"
+    },
+    {
+      title: "A Silent Voice",
+      image: "https://m.media-amazon.com/images/I/81Cn7Usj40L._AC_SY679_.jpg"
+    }
+  ]
+  // You can add more moods later!
 };
+
 
 function suggestMovie() {
   const mood = document.getElementById("moodSelector").value;
   const suggestionDiv = document.getElementById("suggestion");
+  const imageElement = document.getElementById("poster");
 
   if (!mood || !movieSuggestions[mood]) {
     suggestionDiv.innerText = "Please select a valid mood.";
+    imageElement.src = "";
     return;
   }
 
   const movies = movieSuggestions[mood];
   const randomMovie = movies[Math.floor(Math.random() * movies.length)];
 
-  suggestionDiv.innerText = `You should watch: ${randomMovie}`;
+  suggestionDiv.innerText = `You should watch: ${randomMovie.title}`;
+  imageElement.src = randomMovie.image;
 }
+
